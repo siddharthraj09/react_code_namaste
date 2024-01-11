19903b16087e78897f2e2a95ca84b39677f46797
siddharth-plabs <siddharth@plotlabs.com>
2024-01-11 20:55:22 +0530

    the_code

--- ep_2_igniting_our_app/app.js
+++ ep_2_igniting_our_app/app.js
@@ -10,6 +10,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", { id: "headings" }, "I'm an h2 tag"),
   ]),
 ]);
-const heading = React.createElement("h1", { id: "headings" }, "Hello world!"); //object
+// const heading = React.createElement("h1", { id: "headings" }, "Hello world!"); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); //it is responsible for converting it to h1 tag and put it upon root.
